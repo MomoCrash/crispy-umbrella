8e183b4f60f919afb2b706f514ab32c254cbedb2
Ethan <44437470+MomoCrash@users.noreply.github.com>
2026-09-24 16:02:28 +0200

    Update main.cpp

--- testgit/main.cpp
+++ testgit/main.cpp
@@ -3,9 +3,5 @@
 int main()
 {
 
-	std::cout << "rhjklm, comment tu vas ?" << std::endl;
-	std::cout << "Bonjour, comment tu vas zaerazera?" << std::endl;
-	std::cout << "ca va et toi ?" << std::endl;
-
 	return 0; 
 }
